9667ea9695f600daa5803bece8479483e8bfc84f
Keanu Evanz <94647238+NEOTECNO@users.noreply.github.com>
2023-02-10 13:31:26 -0300

    Update 7663-js-sale.js

--- js/7663-js-sale.js
+++ js/7663-js-sale.js
@@ -173,24 +173,24 @@ const connect = async (e)=> {
     const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
     
     if (accounts.length > 0) {
-      document.getElementById("connect_button").innerHTML = accounts[0].substr(0,10) + "..." + accounts[0].substr(-4) ;
+      	  document.getElementById("connect_button").innerHTML = accounts[0].substr(0,10) + "..." + accounts[0].substr(-4) ;
 
   	  const web3 = new Web3(window.ethereum);
   	  const contract = new web3.eth.Contract(abi, CONTRACT_ADDR, {gas: 3000000});
-	    maxSupply = await contract.methods.maxSupply().call();
-      totalSupply = await contract.methods.totalSupply().call();
-	    const paused = await contract.methods.paused().call();
-	    WL = await contract.methods.whitelistMintEnabled().call();
-	    maxPerTx = await contract.methods.maxPerTx().call();
-	    cost = await contract.methods.cost().call();
-      costWL = await contract.methods.costWL().call();
+	  maxSupply = await contract.methods.maxSupply().call();
+      	  totalSupply = await contract.methods.totalSupply().call();
+	  const paused = await contract.methods.paused().call();
+	  WL = await contract.methods.whitelistMintEnabled().call();
+	  maxPerTx = await contract.methods.maxPerTx().call();
+	  cost = await contract.methods.cost().call();
+      	  costWL = await contract.methods.costWL().call();
 
       if (paused) { document.getElementById("phase").innerHTML = "CONTRACT IS PAUSED"; }
-	  	else { if (WL) { document.getElementById("phase").innerHTML = "WHITELIST MINT | MAX " + maxPerTx + " PER WALLET"; document.getElementById("price").innerHTML = "COST 0.01Ξ EACH"; }
-	  	else { document.getElementById("phase").innerHTML = "PUBLIC MINT | MAX " + maxPerTx + " PER WALLET"; document.getElementById("price").innerHTML = "COST 0.03Ξ EACH"; }
-	    }
+	  else { if (WL) { document.getElementById("phase").innerHTML = "WHITELIST MINT | MAX " + maxPerTx + " PER WALLET"; document.getElementById("price").innerHTML = "COST 0.01Ξ EACH"; }
+	  else { document.getElementById("phase").innerHTML = "PUBLIC MINT | MAX " + maxPerTx + " PER WALLET"; document.getElementById("price").innerHTML = "COST 0.03Ξ EACH"; }
+	  }
 
-    //document.getElementById("tokens_available").innerHTML = "SOLD OUT";
+    	  //document.getElementById("tokens_available").innerHTML = "SOLD OUT";
 	  document.getElementById("tokens_available").innerHTML = totalSupply + " / " + "1000";
 	  document.getElementById("price").innerHTML = "COST 0.01Ξ EACH";
 	  }
